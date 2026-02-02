681f789ec8a7a1b7e0ab6c22d32eed74d2bad410
CHICHU <2085219596@qq.com>
2026-02-02 09:49:16 +0800

    2.2 FreeRTOS的浮点数运算修正，以及DSP数学加速库的加入

--- Core/Src/fdcan.c
+++ Core/Src/fdcan.c
@@ -54,7 +54,7 @@ void MX_FDCAN1_Init(void)
   hfdcan1.Init.MessageRAMOffset = 0;
   hfdcan1.Init.StdFiltersNbr = 4;
   hfdcan1.Init.ExtFiltersNbr = 0;
-  hfdcan1.Init.RxFifo0ElmtsNbr = 0;
+  hfdcan1.Init.RxFifo0ElmtsNbr = 10;
   hfdcan1.Init.RxFifo0ElmtSize = FDCAN_DATA_BYTES_8;
   hfdcan1.Init.RxFifo1ElmtsNbr = 0;
   hfdcan1.Init.RxFifo1ElmtSize = FDCAN_DATA_BYTES_8;
